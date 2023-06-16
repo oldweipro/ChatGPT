66d249ec6309b9d20bace73c05416f0d6e265d10
oldweipro <oldweipro@gmail.com>
2023-06-16 18:08:13 +0800

    stable

--- chat/chat.go
+++ chat/chat.go
@@ -138,58 +138,7 @@ func Base(config openai.ClientConfig) {
 const Cookies = ""
 const GtToken = "R0VFAAViYjkwYzQ3N2VmNTRhMmEyAAAKMJrQof9mAC1UOE9KhWoSFu7NtxgygSKIdxMiiTKm1l8fyl5n5/HHIPXeQsX2jCAqKdE7it57lwZFDYoalPKDO4hapSDoWiElGqdztTabuvkFVN6hQ0ZeTlfxvkY59BHodVcANxcGO4e/c+Ndz/u737WHNfsmMCCzwZ6qy36zQ8fooRtiOIaGTcr7p+//NZYrYAIgrkuYGntQ+b3uBFWpwPvHar4VGE0U29NLhRJFKC6lOEV0CDv9rvK9XArtPZkVZBihUdl43OiJ5QW0VVYD6AiiCj5G3GaeNRdFidoScM0QhtsjEhwH9ZP349BdiaNh0hi6nwBz/RQ40GGLOhkbAWIZYq78SmFbB7km/IawfmISLlcHXtxm+cgAjoCgChR9UUBtqyKImPUrRsJMjwaRe9VEcgrr9ns0wSYr5nBBweoMGt0Og6Ui0gMogQkm2Y2lHL6oCxtcqlyWjYwHyAyMcnvxjKiwQDUAgvcnlrrmrVmORw4Y+NeXVGo8/jKTuPGUBd59MCTQt35VW4EEcp8Odgz/+UKIvsVLi+HsuRaEMFU48eM7Ad6YJ3X8VNGBAG6wEz24iNbfsILxyXWwTYJXR/AAqmMWLCNqMnv0PQwtQBebhqflbQTXQzQBiYQK+9fjZFoKrQynKg58ltQ1PDKIAcaazq7p2dz9fZZvS/+g/lnBtXhBOett2q/jj5sQn6sCZ+QPjzI0D2hrwSluAgLShNT+93Zyah41Tr8vIHhRTKYJO7JloUciM85lf5k/ArK3lIAS63kpSzPpW4Kz1d96zSY1VLHxEryq8v6t5nZRLPLmOlT55cZ1fT8pHNQkrwT4FVzYrmieieNycsgUKWbrEHJ+uZpDfHV1dZ162BgADvEKZeP123RBwhFNJAA7Ot3z4UJAYYGCLfpVw1Mhrusd+WCcg0Em+1JBXwBgeU/2UNeGLnYYTiJE0t3KUMvl9zx4cS7PTfCIt3wDhzN2YTSfhfVFmjToYXEeODOdDcBCHWKOD1s758cYLcrlThZwsdIL9r4FpyfZtExx9ToXp6TPxzm4Qnj2IPjQqs3OH+6DkIIqAtK3hPVgq5Yxg7TvVId0ozVRnOXUL434zjrjXF5HfSoutuDSGJjCe+N36goFVfLxL62t/r3RoQmCrEQqvFPjFVnuB6U+i5u2szEWfDtE2v71Bs3N4BoRCIMz3At87nKimUcJX5Ju3plpE1u3j0mi/eivSEzE0P7AFK8tD6d1RwNWmTe2fD6ra89PRnzhhbPXUW2/TfjlrNNmb0qH/09I/WU2TUp1xe/LJOd3KOH4ErUvFzJiJxDYnxs3UKRIBpyGaIqn88PP0QhTfObcyO31hH90h3LjpooT4ngofhTppuqrJpEOuR+4xlNd++6e40BHhnKH8pGRsiFqo7CzYGJJ52ZiMTOl+JfLfz1TGEjro2Jpkg4omPOthYMSrx7g35Qj1BssZqdgpPHYSlgrFoKTkATHuyoiD2bBPTz1yfxtVlRA5/mF+zOq4sH8Cju65gYFKKr4NSXl4jDdRF6FL3zji28gj4ltLNt6IfAw0Ns4iys6QCeF3cKDA+rNsOZPH+PQhCJD9uWbS89JVITeSAFm3eXFgxHxBFj8G5dcwBzAKXnoWffQgYfosZGUSJcaSdZpYZ4waS7FzmZ9W7TPbCCo09hMPU+KAlnIQotYl6rVp1aGSaFR77jjdVITLxlKMYvcDkDtyxgeoJtkUDOT3ymtjNATBkGhyJwHM+JID49dhUnjhGJXnI4k7xIvyHaJQQ2GxoVRjgvUmfkZlfgYdWlsqhdr4iyn7QsGycnfgir/16Pt8yfi4U0D3guNH97kJ/9RELD94Xb93R9/xI9wzYOglnONhOTLmbml1bG7ibJZ5mGwttqdL5EsuVKSKCI509QI8CwtfbDciXMiQY0V+H0K+Rau7fc7x4fKyKvL4Y4b4b+XqfkANst4yb5PBRsoUV0m3jL0zUf5ZQ7LKovvqQbY65RbNeOASl36Y2opPVBr8lnxgUZQFvjUOO38Rng5N3NlZv29RSfYGuXQFqq57gbIsaRhM6dAbgYf+NEUQEwErzjwzEKEYDCo5cY4TBh3hSEDmlaDJVoH3bMO0bukoCtQlMqzHN4EAz0+bhM+mRk725lQzgpWEWVsKyfsmZokWbfMrnNfto5cha0pbfQAbr8Bgd1RdPmscfiGto5JaKdC6fampICVEEm7p6HmPjQaQdTZj8vMbsP1UVhDGDvRoCBb+Lt79DK7rOctIDYqed6Aa7vMBVDBkvZkoZ5TSvtHNGAJAKnJqW0P1PeLLs7rQGQTWVZAtQe93eOecoNah2ibaZKqTAHbbD4/xR0f8XQsZ2gnSd2Zzf5sZe8oyXPkmZ45La/vzFHYQYtqByVULbnNrgFNdDCjX9pbmRv+vxNRblzTYcHM60EE2dyqRurAemXbegEw973ckx9eDhlq3Uxamu1bIbOlQyG0aL1RFbB8JmZFMbOy8Gj7Mw8Mtru8Xd2I9qQQcCu+Zk7o/ShYDgCws/D5EiT7QfWnGFdvL/Ps/neK+M9dOJGE+1KSFU5Kt+EZVQbMOr3ty49j0p3pCP3vFQ505z4mO40XjY/b/MuNzghz5VdE++5KaxeOWSlD+3MXXGWjVPvZQpbqfHkLsUGZap1G++GsGmkyUhsHA6wQQc1dqpztSia8/El5CbSODHLCuoK/ThZS4i2RzkaLXTSjbwExIRdcALYb/ReW3/2D/KUuk047qRGGMbEEEWO2XJPcUbkftd1I2Z0mfRV98x0bIlVlHbUQXsy/lCJN8qZoaW4KpSZZMXMTVGjtVpbAHOWkmsaUoVGL5zz2zwZUgOcyvXpNsN0yg62gmNi3wyqcNvY3se0j0LDWAMxcbHZLbzIcyFqkxIYX+51IEngcCLcyzI7iZvvbSnHGk2sor1s8jVJb4oRE7MU7/jY9v0tY4JMPJPuSC7AtI6FfKlwIsE79w2h8922pqPErMfnmqLfyUetxuetlCbwxT1+YlSoMnumZSMHO8WpkV6X14IIueZltnB6i83VhtEqYm4EN/cykYMiHEiYHkDFgUKSnt/QWZixeZY45YVxIwEaV/tBbxmQhRIDkXYpH4Ajq/jv70cOophMh8OtNjZJYEpzE3Xqk8EAUyO54+gFbQUGih6ckXhjrHk8hGkxsSOjCrDJh9Hbrx+E7T74DsLVrgZ+EgUTPIqhIisTyPzvmn4/Uhfg+lzkSUTK/FxayuCq7blu30wM7KByP3Fw/Rzit+uLvPWsWGUdMIlsiPwNzNiyCKnPScVbQEWfMASpZP3TFSf1gQ20ZGT0FKU0+1UM8L8iFe3G1QQ774YvDmfLFKhnzZpwo+J+0smCbcY+VsOlt0NSz3H6mGpCAVxpcb+AevIonwaZwkLAlGxszEDE4oQIPHFQ/FRBPIlTIJKf/doM/mrxzjI8gmSUXcGr3XtLZSwHu9n5RCTLInazdG8s9i/aRJBXwv4fRDYw="
 
-func XinghuoChatMessage() {
-	client := resty.New()
-	formData := make(map[string]string)
-	formData["chatId"] = "5277689"
-	formData["GtToken"] = GtToken
-	formData["fd"] = ""
-	formData["clientType"] = "2"
-	formData["text"] = text
-	//vcn params is empty;code=11119是什么错误
-	resp, _ := client.R().
-		SetHeader("Accept", "text/event-stream").
-		SetHeader("Referer", "https://xinghuo.xfyun.cn/chat?id="+formData["chatId"]).
-		SetHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1").
-		SetCookie(&http.Cookie{
-			Name:  "ssoSessionId",
-			Value: Cookies,
-		}).
-		SetFormData(formData).SetDoNotParseResponse(true).
-		Post("https://xinghuo.xfyun.cn/iflygpt-chat/u/chat_message/chat")
-	defer func(body io.ReadCloser) {
-		err := body.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(resp.RawBody())
-
-	buf := make([]byte, 1024)
-	textContent := ""
-	for {
-		n, err := resp.RawBody().Read(buf)
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		}
-		if n == 0 {
-			break
-		}
-		msg := string(buf[:n])
-		if strings.Contains(msg, "<end>") {
-			break
-		}
-		result := processMsg(msg)
-		decodeString, msgErr := base64.StdEncoding.DecodeString(result)
-		if msgErr != nil {
-			fmt.Println(msg)
-			fmt.Println("base64解码失败")
-		}
-		textContent += string(decodeString)
-		fmt.Print(string(decodeString))
-	}
-	fmt.Println()
-}
-func XinghuoChatMessageApp() {
+func SparkChatMessageApp() {
 	client := resty.New()
 	formData := make(map[string]string)
 	formData["chatId"] = "5320200"
@@ -203,14 +152,14 @@ func XinghuoChatMessageApp() {
 	formData["text"] = text
 	//vcn params is empty;code=11119是什么错误
 	resp, _ := client.R().
-		//SetHeader("Accept", "text/event-stream").
-		//SetHeader("User-Agent", "okhttp/4.11.0").
-		//SetHeader("Host", "xinghuo.xfyun.cn").
-		//SetHeader("app", "xinghuo").
-		//SetHeader("clientType", "4").
-		//SetHeader("platform", "android").
-		//SetHeader("versionCode", "2023061002").
-		//SetHeader("Accept-Encoding", "gzip").
+		SetHeader("Accept", "text/event-stream").
+		SetHeader("User-Agent", "okhttp/4.11.0").
+		SetHeader("Host", "xinghuo.xfyun.cn").
+		SetHeader("app", "xinghuo").
+		SetHeader("clientType", "4").
+		SetHeader("platform", "android").
+		SetHeader("versionCode", "2023061002").
+		SetHeader("Accept-Encoding", "gzip").
 		SetCookie(&http.Cookie{
 			Name:  "ssoSessionId",
 			Value: Cookies,
@@ -274,7 +223,7 @@ func Loop() {
 			// Print "hello world"
 			text = "行政人事"
 			fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-			XinghuoChatMessageApp()
+			SparkChatMessageApp()
 			// Sleep for 10 minutes
 			time.Sleep(10 * time.Minute)
 		}
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "github.com/oldweipro/chatgpt/chat"
 
 func main() {
-	chat.RunCmdChatGPT(chat.XinghuoChatMessage)
+	chat.RunCmdChatGPT(chat.SparkChatMessageApp)
 	//gin_server.GinServer()
 }
